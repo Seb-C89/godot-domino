4800d84d466736cfc0ae0318146e3e74e5024ab3
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-07-02 19:42:20 +0200

    add flip() feature

--- scn/domino.gd
+++ scn/domino.gd
@@ -1,9 +1,7 @@
 extends MeshInstance
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var __status = 1 #face visible (1) / face hidden (-1)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,5 +28,16 @@ func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx
 	if event is InputEventMouseButton:
 		if event.doubleclick:
 			print("click click")
-	if event is InputEventMouseMotion:
-		print("move")
+			flip()
+#	if event is InputEventMouseMotion:
+#		print("move")
+
+
+func flip():
+	if __status == 1:
+		$Tween.interpolate_property($".", "rotation_degrees", null, Vector3(0, 0, 180), 0.25)
+	elif __status == -1:
+		$Tween.interpolate_property($".", "rotation_degrees", null, Vector3(0, 0, 0), 0.25) # vers la gauche
+#		$Tween.interpolate_property($".", "rotation_degrees", Vector3(0, 0, -180), Vector3(0, 0, 0), 0.25) # vers la droite
+	$Tween.start()
+	__status *= -1
