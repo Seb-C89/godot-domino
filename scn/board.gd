00fb22d7b2556933b6da094996ecee56cba8cb7d
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-07-16 21:02:58 +0200

    duplique material for each domino instance

--- scn/board.gd
+++ scn/board.gd
@@ -24,13 +24,18 @@ func _ready():
 			__board[rowID][colID] = domino_pack.instance()
 			add_child(__board[rowID][colID])
 			
-	#Measure One
+	#Measure One (Measure must be done after the instance is added to scene. see comment below)
 	var mesh_size = __board[0][0].get_node("mesh_domino").get_transformed_aabb().size
 	
-	#Position all
+	#For each domino
 	for rowID in len(__board): # for rowID, row in enumarate(board):
 		for colID in len(__board[rowID]):
+			#Position
 			__board[rowID][colID].global_translate(Vector3(colID, rowID, 0) * (mesh_size + Vector3(__tiles_offset, __tiles_offset, __tiles_offset)))
+			#Duplique material (else all instance share same params like uv_coord, color, ...)
+			__board[rowID][colID].get_node("mesh_domino/mesh_face").set_surface_material(0, __board[0][0].get_node("mesh_domino/mesh_face").get_surface_material(0).duplicate())
+			#Set_face()
+			__board[rowID][colID].set_face(rowID+colID)
 	
 #	https://discord.com/channels/667748228212457482/677940032535003136/861790618799702016
 #	domino_pack = preload("res://scn/domino.tscn")
