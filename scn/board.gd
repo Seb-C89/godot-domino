aac951a9d5fb77e9dcad0697bdfcb97119164975
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-08-11 23:28:56 +0200

    [Add] chain_timeout signal [Change] domino's signal use domino's ref instead col and row index. Indexs are retrieve using domino's position in get_domino_index_from_position()

--- scn/board.gd
+++ scn/board.gd
@@ -11,6 +11,7 @@ var __board = []
 var __tiles_offset = 0
 var __alea = []
 var __board_size = 0
+var __domino_size
 
 var chain = []
 
@@ -50,32 +51,35 @@ func generate():
 			add_child(__board[rowID][colID])
 			
 	#Measure One (Measure must be done after the instance is added to scene. see comment below)
-	var mesh_size = __board[0][0].get_node("mesh_domino").get_transformed_aabb().size
+	__domino_size = __board[0][0].get_node("mesh_domino").get_transformed_aabb().size
 	
 	#For each domino
 	for rowID in len(__board): # for rowID, row in enumarate(board):
 		for colID in len(__board[rowID]):
 			#Position
-			__board[rowID][colID].global_translate(Vector3(colID, rowID, 0) * (mesh_size + Vector3(__tiles_offset, __tiles_offset, __tiles_offset)))
+			__board[rowID][colID].global_translate(Vector3(colID, rowID, 0) * (__domino_size + Vector3(__tiles_offset, __tiles_offset, __tiles_offset)))
 			#Duplique material (else all instance share same params like uv_coord, color, ...)
 			__board[rowID][colID].get_node("mesh_domino/mesh_face").set_surface_material(0, __board[0][0].get_node("mesh_domino/mesh_face").get_surface_material(0).duplicate())
 			#Set_face()
 			__board[rowID][colID].set_face(__alea[rowID+colID])
 			#Connect input event
-			__board[rowID][colID].get_node("Area").connect("input_event", self, "on_Domino_input_event", [rowID, colID])
+#			__board[rowID][colID].get_node("Area").connect("input_event", self, "on_Domino_input_event", [rowID, colID])
+			__board[rowID][colID].get_node("Area").connect("input_event", self, "on_Domino_input_event", [__board[rowID][colID]])
 	
 	__board_size = __board[__rows-1][__cols-1].get_node("mesh_domino").get_transformed_aabb().end - __board[0][0].get_node("mesh_domino").get_transformed_aabb().position
 	print("board", __board_size)
-	print("mesh", mesh_size)
+	print("mesh", __domino_size)
 	print("cols", __cols, "rows", __rows)
 
 
-func on_Domino_input_event(camera, event, click_position, click_normal, shape_idx, rowID, colID):
+#func on_Domino_input_event(camera, event, click_position, click_normal, shape_idx, rowID, colID):
+func on_Domino_input_event(camera, event, click_position, click_normal, shape_idx, domino):
 	if event is InputEventMouseButton:
 		if event.doubleclick:
-			print("face", __board[rowID][colID].__face)
-			__board[rowID][colID].flip()
-			chain_add(rowID, colID)
+#			print("face", __board[rowID][colID].__face)
+			get_domino_index_from_position(domino)
+#			__board[rowID][colID].flip()
+#			chain_add(rowID, colID)
 
 
 func chain_add(rowID, colID):
@@ -95,6 +99,19 @@ func chain_add(rowID, colID):
 		print("first")
 		chain.append(__board[rowID][colID])
 		emit_signal("chain_grow", chain)
+
+
+func chain_timeout():
+	chain.clear()
+	emit_signal("chain_lost", chain)
+	print("chain timeout")
+
+
+func get_domino_index_from_position(domino):
+	var x = domino.get_translation().x / (__domino_size.x + __tiles_offset)
+	var y = domino.get_translation().y / (__domino_size.y + __tiles_offset)
+	return[round(x), round(y)]
+
 
 #	TODO chain as class ?
 
